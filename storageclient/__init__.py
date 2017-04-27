6f8c1b6b9241816fbea58d475d8046a008cf9800
jesuspg <jesuspg@uchicago.edu>
2017-04-27 20:50:11 +0000

    fix(minor): remove commented code

--- storageclient/__init__.py
+++ storageclient/__init__.py
@@ -3,10 +3,6 @@ from cleversafe import CleversafeClient
 
 def get_client(config=None, backend=None):
     try:
-        # if backend == 'cleversafe':
-        #     return CleversafeClient(config)
-        # if backend == 'ceph':
-        #     raise NotImplementedError()
         clients = {'cleversafe': CleversafeClient}
         return clients[backend](config)
     except KeyError as ex:
