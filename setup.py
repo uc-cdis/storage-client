f0b1c0e12632cc5c3ad481f872f4d463eceb36ef
Rudyard Richter <rudyardrichter@uchicago.edu>
2017-09-20 15:57:55 -0500

    chore(requirements): fix pbr version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'boto==2.46.1',
         'botocore==1.5.35',
-        'requests==2.13.0',
+        'requests>=2.13.0',
         's3transfer==0.1.10',
         'jmespath==0.9.2',
         'pbr==2.0.0',
