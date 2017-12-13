b4ae32788754bf399a13ee67eeecc822cabd85ae
Thanh Nguyen <thanhnd@uchicago.edu>
2017-12-13 15:26:18 -0600

    fix(boto): flexible version

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     license="Apache",
     packages=find_packages(),
     install_requires=[
-        'boto==2.36.0',
-        'botocore==1.5.35',
+        'boto>=2.36.0',
+        'botocore>=1.5.35',
         'requests==2.13.0',
         's3transfer==0.1.10',
         'jmespath==0.9.2',
