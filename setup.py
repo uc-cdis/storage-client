dc64f4e695a4cbcb7bfb043aeb9b4eb244716723
BinamB <binam@binam-XPS-13-9360.cdis>
2020-07-07 11:30:57 -0500

    s3transger

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "boto>=2.36.0,<3.0.0",
         "botocore>=1.7,<1.13.0",
         "requests>=2.5.2,<3.0.0",
-        "s3transfer<0.4.0,>=0.3.0",
+        "s3transfer<0.3.0,>=0.2.0",
         "jmespath==0.9.2",
         "pbr==2.0.0",
         "cdislogging",
