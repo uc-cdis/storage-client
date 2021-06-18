4d39265d6e478acd5e1afe6e5dc722418f887d78
Pauline Ribeyre <ribeyre@uchicago.edu>
2021-06-18 14:12:54 -0500

    Accept gen3cirrus >=1.0.0, <3.0.0 (#32)

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ setup(
     install_requires=[
         "boto>=2.36.0,<3.0.0",
         "botocore>=1.7,<1.13.0",
+        "urllib3>=1.20,<1.26",  # as required by botocore-1.12.253
+        "six>=1.13.0",  # as required by google-api-core
         "requests>=2.5.2,<3.0.0",
         "s3transfer<0.3.0,>=0.2.0",
         "jmespath==0.9.2",
         "pbr==2.0.0",
-        "cdislogging",
-        "gen3cirrus>=1.0.0,<2.0.0",
+        "cdislogging>=1.0.0,<2.0.0",
+        "gen3cirrus>=1.0.0,<3.0.0",
     ],
 )
