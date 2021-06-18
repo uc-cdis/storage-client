0b9ed424826690cf21eba104a2260c66ffff3cc1
Pauline <ribeyre@uchicago.edu>
2021-06-18 13:20:16 -0500

    urllib3>=1.20

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     install_requires=[
         "boto>=2.36.0,<3.0.0",
         "botocore>=1.7,<1.13.0",
+        "urllib3>=1.20,<1.26",  # as required by botocore-1.12.253
         "requests>=2.5.2,<3.0.0",
         "s3transfer<0.3.0,>=0.2.0",
         "jmespath==0.9.2",
