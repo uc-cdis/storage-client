007ee29346f10134f0e68c9d64335a1d0731a331
Alexander VT <alexander.m.vantol@gmail.com>
2021-11-02 14:11:05 -0500

    chore(deps): update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "urllib3>=1.20",  # as required by botocore-1.12.253
         "six>=1.13.0",  # as required by google-api-core
         "requests>=2.5.2",
-        "s3transfer<0.3.0",
+        "s3transfer",
         "jmespath==0.9.2",
         "pbr==2.0.0",
         "cdislogging>=1.0.0",
